81aafd6f3f71cb10bfb60b89b75c8ebead45c702
lexuv2 <35073872+lexuv2@users.noreply.github.com>
2025-03-28 21:42:54 +0100

    working version

--- key_generator/key_gen.py
+++ key_generator/key_gen.py
@@ -5,7 +5,9 @@ import tkinter
 from usbmonitor import USBMonitor
 import time
 from Crypto.Util.Padding import pad
-from tkinter import simpledialog, messagebox
+from tkinter import simpledialog, messagebox, filedialog
+import psutil
+import os
 tk = tkinter.Tk()
 
 tk.withdraw()
@@ -30,6 +32,47 @@ def on_usb_connected(device_id, device_info):
 def save_key_to_file(key, file_path):
     with open(file_path, 'wb') as f:
         f.write(key)
+
+def get_usb_devices():
+    devices = []
+    if os.name == 'nt':  # Windows
+        for partition in psutil.disk_partitions():
+            if 'removable' in partition.opts:
+                usb_path = partition.mountpoint
+                devices.append(usb_path)
+    else:  # Linux
+        media_path = "/run/media/" + os.getlogin()
+        if os.path.exists(media_path):
+            devices = [os.path.join(media_path, d) for d in os.listdir(media_path) if os.path.isdir(os.path.join(media_path, d))]
+    return devices
+
+
+def save_key_to_usb(key):
+    devices = get_usb_devices()
+    if not devices:
+        messagebox.showerror("error", "Nie znaleziono podłączonego pendrive'a.")
+        return
+    for device in devices:
+        key_path = os.path.join(device, "private_encrypted.pem")
+        with open(key_path, 'wb') as f:
+            f.write(key)
+        print(f"Zapisano klucz na {device}")
+
+
+def save_key_to_usb_with_ui(key):
+    devices = get_usb_devices()
+    if not devices:
+        messagebox.showerror("Błąd", "Nie znaleziono podłączonego pendrive'a.")
+        return
+    device = filedialog.askdirectory(title="Wybierz urządzenie USB", initialdir=devices[0])
+    if not device:
+        messagebox.showerror("Błąd", "Nie wybrano urządzenia USB.")
+        return
+    filename = "klucz_prywatny_zaszyfrowany.pem"
+    key_path = os.path.join(device, filename)
+    with open(key_path, 'wb') as f:
+        f.write(key)
+    messagebox.showinfo("Sukces", f"Zapisano klucz na {key_path}")
 
 
 def show_popup(message):
@@ -61,4 +104,9 @@ if not pin:
 encrypted_key = encrypt_private_key(keys.export_key(), pin)
 print(encrypted_key)
 save_key_to_file(encrypted_key, "private_encrypted.pem")
+
+save_to_usb = messagebox.askyesno("Zapisz na USB", "Czy chcesz zapisać zaszyfrowany klucz na urządzeniu USB?")
+if save_to_usb:
+    save_key_to_usb_with_ui(encrypted_key)
+
 monitor.stop_monitoring()
